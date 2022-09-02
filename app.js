7b8037789fd949391b02f1fcc406ea5e817c65dd
Jaymac01 <jayantkumar@validatemeinc.com>
2022-09-02 11:43:32 +0530

    changes

--- app.js
+++ app.js
@@ -1,24 +1,61 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const cors = require("cors");
+const fs = require("fs");
+
 const db = "mongodb+srv://jaymac:jayantkumar7722@cluster0.nwvbroe.mongodb.net/p2p?retryWrites=true&w=majority";
 const testimonialschema = require('./testimonialschema.js')
 mongoose.connect(db).then(() => {
     console.log('Connection successful');
 }).catch((err) => { console.log('error'); })
 
-
-testimonialschema
-
-
 const testimonial = new mongoose.model("Testimonial", testimonialschema);
+
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, "uploads");
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, file.originalname);
+//     },
+//   });
+
+
+//   const upload = multer({ storage: storage });
+
+
+
+const storage = multer.diskStorage({
+    //destination for files
+    destination: function (request, file, callback) {
+      callback(null, '../frontend/public/uploads');
+    },
+  
+    //add back the extension
+    filename: function (request, file, callback) {
+      callback(null, Date.now() + file.originalname);
+    },
+  });
+
+  const upload = multer({
+    storage: storage,
+    
+  });
+  
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/get/testimonials', async (req, res) => {
 
     try {
-        let newvalue = new testimonial({ name: "jayant" });
-        newvalue.save();
+      
         const data = await testimonial.find();
 
         res.send(data).status(200)
@@ -30,23 +67,25 @@ app.get('/get/testimonials', async (req, res) => {
 
 })
 
-app.post('/add/testimonial', async (req, res) => {
+
+
+app.post('/add/testimonial', upload.single('photo'), async (req, res) => {
 
     try {
 
         const obj = {
-            photo: req.body.photo,
-            name: req.body.name,
-            post: req.body.post,
-            active: req.body.active,
+            photo :  !(req.file.filename) ? undefined : req.file.filename,
+            name : req.body.name ,
+            post: req.body.post ,
+            active: req.body.active ,
             testimonial_Description: req.body.testimonial_Description
         }
- console.log(obj)
+        console.log(obj,"hello")
 
 
 
-        let newvalue = new testimonial(obj);
-         newvalue.save().then(() => { res.send({ "msg": "testimonial added successfully" }).status(200) }).catch((error) => { console.log(error); });
+        const newvalue = new testimonial(obj);
+        newvalue.save().then(() => { res.send({ "msg": "testimonial added successfully" }).status(200) }).catch((error) => { console.log(error); });
     }
     catch (error) {
         res.send(error).status(400)
@@ -61,7 +100,7 @@ app.put('/edit/testimonial/:testimonial_ID', async (req, res) => {
         const id = req.params.testimonial_ID;
 
         const obj = {
-            photo: req.body.photo,
+            photo: req.file.filename,
             name: req.body.name,
             post: req.body.post,
             active: req.body.active,
@@ -69,7 +108,7 @@ app.put('/edit/testimonial/:testimonial_ID', async (req, res) => {
         }
 
         await testimonial.updateOne({ testimonial_ID: id }, obj).then(() => { res.send({ "msg": "testimonial edited successfully" }).status(200) }).catch((error) => { console.log(error); });
-        
+
     }
     catch (error) {
         res.send(error).status(400)
